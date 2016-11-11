23c36a3035918516c18d646fac51ecfaff01aaf5
keyan vakil <keyanv@bu.edu>
2016-11-10 23:59:42 -0500

    Add comments; Add API checks;

--- Whoogle/app/controllers/welcome_controller.rb
+++ Whoogle/app/controllers/welcome_controller.rb
@@ -1,4 +1,6 @@
 class WelcomeController < ApplicationController
   def index
+  	# the index function is the WelcomeController should do nothing
+  	# TODO find out if this is necessary?
   end
 end
