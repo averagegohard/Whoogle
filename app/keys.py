c2f930bb9777e9e3dde11a825300e5b7c1e1cf82
omayr-abd <omayr.abd@gmail.com>
2016-12-04 15:29:38 -0500

    Update keys.py

--- app/keys.py
+++ app/keys.py
@@ -1,4 +1,4 @@
 # this is where you should put the keys
-FB_APP_ID = ''
-FB_APP_SECRET = ''
-FB_APP_NAME = ''
+FB_APP_ID = '1814067055534233'
+FB_APP_SECRET = '5f5e523776050980e4ec5ab9962df8ea'
+FB_APP_NAME = 'whoogle'
